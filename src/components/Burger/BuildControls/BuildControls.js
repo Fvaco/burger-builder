c4fd978d734ced38f567d918b0c4120514216625
Iván Hernández Ortega <ivan@wupplier.com>
2018-12-06 16:54:39 +0000

    :sparkles: Build Controls implementation. Working version

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,25 @@
 import React from "react";
 
+import classes from "./BuildControls.css";
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" }
+];
+
 const buildControls = props => (
-  <div>
-    
+  <div className={classes.BuildControls}>
+    {controls.map(control => (
+      <BuildControl
+        added={() => props.ingredientAdded(control.type)}
+        removed={() => props.ingredientRemoved(control.type)}
+        key={control.label}
+        label={control.label}
+      />
+    ))}
   </div>
 );
 
